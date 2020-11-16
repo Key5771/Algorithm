3b9b3650b5fc9f3213d95edabb99e29a83b236ae
key5771 <ksj57715@hanmail.net>
2020-11-16 14:34:54 +0900

    Baekjoon 2752번

---
+++ Baekjoon/Baekjoon 2752/Baekjoon 2752/Baekjoon 2752/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  Baekjoon 2752
+//
+//  Created by 김기현 on 2020/11/16.
+//
+
+let input = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
+print("\(input[0]) \(input[1]) \(input[2])")
